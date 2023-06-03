9c179457a78513dd4591fed903a895b9642aaf16
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-06-03 17:33:40 -0300

    Quarto commit"

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import { ValidationPipe } from "@nestjs/common";
 (async() => {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 })();
